709a2e128580f704dc17aa5dee55f18ff861d9bc
EternalEnvy <lee.southerst@gmail.com>
2016-10-04 19:49:25 -0400

    Directory setup

--- SmackBrosClient2/DirectX/Meshes.cs
+++ SmackBrosClient2/DirectX/Meshes.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Drawing;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using SharpDX;
+using SharpDX.D3DCompiler;
 using SharpDX.Direct3D;
-using Direct3D = SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
 using Assimp;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
 
 namespace SmackBrosClient2.DirectX
 {
     class Meshes : Window
     {
-
     }
 }
